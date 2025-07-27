ff1bfbee265577c7ab9a596ff25efed07930d944
KoPyaeCola898 <ko512894@gmail.com>
2025-07-27 10:31:24 +0630

    Time: 4:49:51 Add Login Design

---
+++ SMMS/Login.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SMMS
+{
+    public partial class Login : Form
+    {
+        SqlConnection cn = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+        DBConnect dbcon = new DBConnect();
+        SqlDataReader dr;
+
+        public Login()
+        {
+            InitializeComponent();
+            cn = new SqlConnection(dbcon.myConnection());
+        }
+
+        private void picClose_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Exit Application?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
